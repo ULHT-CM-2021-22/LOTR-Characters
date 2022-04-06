766c2513cb200af0690f4c6f1467d013c09589c1
Pedro Alves <pedro.alves@ulusofona.pt>
2022-04-06 19:13:32 +0100

    Improve list presentation

--- app/src/main/java/pt/ulusofona/cm/lotrcharacters/activities/CharactersListActivity.kt
+++ app/src/main/java/pt/ulusofona/cm/lotrcharacters/activities/CharactersListActivity.kt
@@ -1,7 +1,9 @@
 package pt.ulusofona.cm.lotrcharacters.activities
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import pt.ulusofona.cm.lotrcharacters.R
 import pt.ulusofona.cm.lotrcharacters.databinding.ActivityCharactersListBinding
@@ -24,6 +26,8 @@ class CharactersListActivity : AppCompatActivity() {
 
         binding.charactersListRv.layoutManager = LinearLayoutManager(this)
         binding.charactersListRv.adapter = CharacterListAdapter(characters)
+        // add a divider between items of the list
+        binding.charactersListRv.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
 
     }
 }
